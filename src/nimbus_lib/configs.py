2dc7db968f6bbfe6979de295f250b77263ea4535
Matt White <mail@matt-w.net>
2023-12-15 14:51:45 -0700

    update pydantic

--- src/nimbus_lib/configs.py
+++ src/nimbus_lib/configs.py
@@ -3,7 +3,8 @@ from typing import Iterable
 from enum import Enum, unique
 
 
-from pydantic import BaseSettings, BaseModel, Field
+from pydantic import ConfigDict, BaseModel, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 @unique
@@ -21,11 +22,9 @@ class SecretConfig(BaseModel):
     name: str
     region: str
 
-    class Config:
-        min_anystr_length = 1
-        error_msg_templates = {
-            "value_error.any_str.min_length": "min_length:{limit_value}",
-        }
+    model_config = ConfigDict(
+        str_min_length=1,
+    )
 
 
 class DomainConfig(BaseModel):
@@ -95,15 +94,13 @@ class FargateConfig(BaseSettings):
             return (self.external_http_port, self.external_https_port)
         return (self.external_http_port,)
 
-    class Config:
-        case_sensitive = False
-        min_anystr_length = 1
-        error_msg_templates = {
-            "value_error.any_str.min_length": "min_length:{limit_value}",
-        }
-        env_file = os.environ.get("ENVFILE", ".env")
-        env_file_encoding = "utf-8"
-        env_nested_delimiter = "__"
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+        str_min_length=1,
+        env_file=os.environ.get("ENVFILE", ".env"),
+        env_file_encoding="utf-8",
+        env_nested_delimiter="__",
+    )
 
 
 __all__ = [
